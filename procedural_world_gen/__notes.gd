18500e0c885b63d8b6c289bcfe61cdc47b310a36
Rafael <rafaeldecastro.dev@gmail.com>
2025-10-24 15:00:49 -0300

    water layer terrain and dark ground bg

--- procedural_world_gen/__notes.gd
+++ procedural_world_gen/__notes.gd
@@ -5,4 +5,5 @@ extends Node
 2. No atributo Noise, criar um novo FastNoiseLite
 3. Color Ramp para melhor visualização dos valores (Interpolation > Mode constant)
 4. Ajustar o valor de frequency = zoom in/out do noise, aumentando/diminuindo as land masses (menos é mais land)
+5. Ajustar o z-index das layers (Ordering > Z Index)
 """
--- procedural_world_gen/world_manager.gd
+++ procedural_world_gen/world_manager.gd
@@ -6,26 +6,34 @@ extends Node
 var noise: Noise;
 var noise_seed: int = 1;
 var noise_value_array: Array[float] = [];
-var width: int = 480;
+var width: int = 180;
 var height: int = 270;
 var x_start_render: Vector2i = Vector2i.ZERO;
 var y_start_render: Vector2i = Vector2i.ZERO;
 
 var source_id: int = 0;
 var water_atlas_coord: Vector2i = Vector2i(1,2);
-var land_atlas_coord: Vector2i = Vector2i(0,0);
+var ground_atlas_coord: Vector2i = Vector2i(0,0);
 var grass_atlas_coord: Vector2i = Vector2i(1,0);
 var trees_atlas_coord: Vector2i = Vector2i(8,0);
 
+var water_layer_terrain_set_index: int = 0;
+var water_layer_terrain_index: int = 0;
+
+var water_tiles_array: Array[Vector2i] = [];
+var ground_tiles_array: Array[Vector2i] = [];
+var grass_tiles_array: Array[Vector2i] = [];
+var trees_tiles_array: Array[Vector2i] = [];
+
 @onready var overworld: Node2D = $"../Overworld";
-@onready var layer_ground: TileMapLayer = $"../Overworld/LayerGround";
-@onready var layer_water: TileMapLayer = $"../Overworld/LayerWater";
-@onready var layer_grass: TileMapLayer = $"../Overworld/LayerGrass";
-@onready var layer_trees: TileMapLayer = $"../Overworld/LayerTrees";
+@onready var water_layer: TileMapLayer = $"../Overworld/LayerWater";
+@onready var ground_layer: TileMapLayer = $"../Overworld/LayerGround";
+@onready var grass_layer: TileMapLayer = $"../Overworld/LayerGrass";
+@onready var trees_layer: TileMapLayer = $"../Overworld/LayerTrees";
 
 func _ready() -> void:
 	randomize();
-	noise_seed = randi();
+	#noise_seed = randi();
 	noise = noise_height_texture.noise;
 	noise.seed = noise_seed;
 
@@ -34,21 +42,40 @@ func _ready() -> void:
 	@warning_ignore("integer_division")
 	y_start_render = Vector2i(height * -1 / 2, height / 2);
 
+	water_layer.z_index = -1;
+	ground_layer.z_index = -1;
+	grass_layer.z_index = -1;
 	generate_world();
 #}
 
 func generate_world() -> void:
+
 	noise_value_array = [];
+
 	for x in range(x_start_render.x, x_start_render.y):
 		for y in range(y_start_render.x, y_start_render.y):
+
 			var noise_value = noise.get_noise_2d(x,y);
 			noise_value_array.append(noise_value);
+
 			if noise_value >= 0.0: # place land
-				layer_ground.set_cell( Vector2i(x,y), source_id, land_atlas_coord);
+				## Normal circunstances ground is above water, but this time I want all ground bellow
+				#ground_tiles_array.append(Vector2i(x,y));
+				#ground_layer.set_cell( Vector2i(x,y), source_id, ground_atlas_coord);
+
 				if noise_value > 0.1:
-					layer_grass.set_cell( Vector2i(x,y), source_id, grass_atlas_coord);
+					grass_tiles_array.append(Vector2i(x,y));
+					grass_layer.set_cell( Vector2i(x,y), source_id, grass_atlas_coord);
+
 				if noise_value > 0.2:
-					layer_trees.set_cell( Vector2i(x,y), source_id, trees_atlas_coord);
+					trees_tiles_array.append(Vector2i(x,y));
+					trees_layer.set_cell( Vector2i(x,y), source_id, trees_atlas_coord);
 			elif noise_value < 0.0:  # place water
-				layer_water.set_cell( Vector2i(x,y), source_id, water_atlas_coord);
+				water_tiles_array.append(Vector2i(x,y));
+			#} endif
+
+		#} endfor y
+	#} endfor x
+
+	water_layer.set_cells_terrain_connect(water_tiles_array, water_layer_terrain_set_index, water_layer_terrain_index);
 #}
